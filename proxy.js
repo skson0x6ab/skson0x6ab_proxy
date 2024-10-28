36307d741a07ffa607caa199552000883d06da6b
skson0x6ab <skson0x6ab@gmail.com>
2024-10-28 17:51:02 +0900

    console.log 추가

--- proxy.js
+++ proxy.js
@@ -15,9 +15,12 @@ const imageProcessingMiddleware = responseInterceptor(async (responseBuffer, pro
     const imageTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
     if (imageTypes.includes(proxyRes.headers['content-type'])) {
         try {
+            console.log('이미지 처리 시작...');
             const image = await Jimp.read(responseBuffer);
             image.flip(true, false).sepia().pixelate(5);
-            return image.getBufferAsync(Jimp.AUTO);
+            const buffer = await image.getBufferAsync(Jimp.AUTO);
+            console.log('이미지 처리 완료');
+            return buffer;
         } catch (err) {
             console.error('이미지 처리 오류: ', err);
             return responseBuffer;
@@ -28,7 +31,7 @@ const imageProcessingMiddleware = responseInterceptor(async (responseBuffer, pro
 
 // 첫 번째 프록시 미들웨어 설정 (test1)
 app.use('/test1', createProxyMiddleware({
-    target: 'https://skson-dashboard.vercel.app/',
+    target: 'https://skson-dashboard.vercel.app',
     changeOrigin: true,
     selfHandleResponse: true,
     timeout: 60000, // 60초로 설정
