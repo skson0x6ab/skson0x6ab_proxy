6f5bdb6da6cc69ba10095dce5bbed4cb334e3483
skson0x6ab <skson0x6ab@gmail.com>
2024-10-28 17:29:42 +0900

    jimp 라이브러리 추가

--- proxy.js
+++ proxy.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
+const Jimp = require('jimp');
 const app = express();
 
 // CORS 헤더 추가
@@ -9,16 +10,37 @@ app.use((req, res, next) => {
     next();
 });
 
-// 프록시 미들웨어 설정
-app.use('/test1', createProxyMiddleware({
-        target: 'https://skson-dashboard.vercel.app/#/dashboard',
-        changeOrigin: true
-    }));
+// 이미지 처리 함수
+const imageProcessingMiddleware = responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+    const imageTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
+    if (imageTypes.includes(proxyRes.headers['content-type'])) {
+        try {
+            const image = await Jimp.read(responseBuffer);
+            image.flip(true, false).sepia().pixelate(5);
+            return image.getBufferAsync(Jimp.AUTO);
+        } catch (err) {
+            console.error('이미지 처리 오류: ', err);
+            return responseBuffer;
+        }
+    }
+    return responseBuffer;
+});
 
+// 첫 번째 프록시 미들웨어 설정 (test1)
+app.use('/test1', createProxyMiddleware({
+    target: 'https://skson-dashboard.vercel.app/',
+    changeOrigin: true,
+    selfHandleResponse: true,
+    onProxyRes: imageProcessingMiddleware
+}));
+
+// 두 번째 프록시 미들웨어 설정 (test2)
 app.use('/test2', createProxyMiddleware({
-        target: 'https://github.com/skson0x6ab',
-        changeOrigin: true
-    }));
+    target: 'https://github.com/skson0x6ab/',
+    changeOrigin: true,
+    selfHandleResponse: true,
+    onProxyRes: imageProcessingMiddleware
+}));
 
 app.listen(5000, () => {
     console.log('프록시 서버가 http://localhost:5000 에서 실행 중입니다.');
